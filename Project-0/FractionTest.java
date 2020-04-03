8efc80c5105d5b869c1a1649d4167c3414f97006
Karolina K <63045938+FlamingLlama@users.noreply.github.com>
2020-04-03 16:22:27 +0100

    Test function of Fraction.java

---
+++ Project-0/FractionTest.java
@@ -0,0 +1,21 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf610
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\csgray\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 public class FractionTest \{\
+    public static void main(String args[]) \{\
+\
+        Fraction f = new Fraction(10,20);\
+        Fraction g = new Fraction(3,4);\
+\
+        System.out.println(f);\
+        System.out.println(g);\
+\
+        System.out.println(f.add(g));\
+        System.out.println(f.subtract(g));\
+\
+    \}\
+\}}
